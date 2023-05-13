140591ff30fb83db1877642a12ece1eaa295c926
Daiana <daianaroxys@gmail.com>
2023-05-13 20:48:56 +0200

    cabecera para aceptar peticiones de cualquier dominio

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const server = express()
 
+server.use((req,res,next) => {
+    res.header('Access-Control-Allow-Origin', '*')
+    next()
+})
+
 server.get('/', (req,res) => {
     let [r,g,b] = [0,0,0].map(() => Math.floor(Math.random()*256))
     res.json({r,g,b})
